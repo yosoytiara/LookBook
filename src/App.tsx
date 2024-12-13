885a71cd56b40f6379f37843cf22a5d5f0e7922d
“Tiara <tiaravan234@gmail.com>
2024-12-13 09:39:08 -0500

    outfits completed

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
     <div>
       <h1 id='lookbook'>LookBook</h1>
       <InputBox></InputBox>
-      {/* <Outfits></Outfits> */}
+      <Outfits></Outfits>
     </div>
   );
 };
