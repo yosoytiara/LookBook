0ab907546559bb49580fb13fa0b926fa44b41b44
“Tiara <tiaravan234@gmail.com>
2025-01-13 19:55:03 -0500

    creating backend server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,30 @@
 import express from 'express';
-import cors from 'cors';
-import records from './routes/record.js';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
-const PORT = process.env.PORT || 5050;
+dotenv.config();
+
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use('/record', records);
+app.post('/products', async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.category || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: 'Please provide all fields' });
+  }
+  const newProduct = new Product(product);
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+  console.log("Error in creating product:", error.message);
+res.status(500).json({success: false, message:"Server Error"}) ;
+}
+});
+
+app.listen(3030, () => {
+  connectDB();
+  console.log('Server started at http://localhost:3030/');
 });
