9193c0ff5bc47b5c99b9b9550175bd446c0b9c15
“Tiara <tiaravan234@gmail.com>
2025-01-14 10:38:11 -0500

    setting up routes and controller

--- backend/server.js
+++ backend/server.js
@@ -2,27 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
+
+import productRoutes from "./routes/product.rout.js"
 dotenv.config();
 
 const app = express();
 
-app.post('/products', async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.category || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Please provide all fields' });
-  }
-  const newProduct = new Product(product);
+app.use("/products", productRoutes);
 
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-  console.log("Error in creating product:", error.message);
-res.status(500).json({success: false, message:"Server Error"}) ;
-}
-});
 
 app.listen(3030, () => {
   connectDB();
