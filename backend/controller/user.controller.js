20514a057b8f19869754a18f12704e2728c84eb0
Tiara <114452621+yosoytiara@users.noreply.github.com>
2025-04-30 20:24:38 -0400

    working on user controller

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,3 +1,24 @@
 import User from '../models/user.model';
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+export const Signup = async (req, res) =>{
+    const {username, password} = req.body;
+    
+    try{
+
+    }catch (error){
+
+    }
+
+};
+
+export const Login = async (req, res) => {
+    try {
+    } catch (error) {
+
+    }
+};
 
 
