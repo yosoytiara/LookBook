2d32ff3878fd7b56ae83fa63e3c4caba8549678a
Tiara <114452621+yosoytiara@users.noreply.github.com>
2026-02-15 20:11:18 -0500

    button functionality for saving outfits in process

--- frontend/src/pages/Generator.tsx
+++ frontend/src/pages/Generator.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Outfits from '../components/Outfits';
 
+const saveOutfit = async () => {
+  const token = localStorage.getItem('token');
+
+  try {
+    await axios.post('http://localhost:3030/outfits', selectedItem, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } catch (err) {
+    console.error('Error saving outfit', err);
+  }
+};
 
 export default function Generator() {
   return (
     <div className='Generator'>
       <Outfits />
+      <button onClick={saveOutfit}>Save Outfit</button>;
     </div>
   );
 }
